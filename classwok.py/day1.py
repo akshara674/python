a41762c69c9baa533542ade621b4b2a6d217db2e
akshara <aksharapsreekumar@gmail.com>
2025-10-15 08:49:25 +0530

    updated home worl/day2 home.py

---
+++ classwok.py/day1.py
@@ -0,0 +1,23 @@
+Apple = 15.5
+Orange = 20
+Grape = 10.25
+
+Total = Apple + Orange + Grape
+
+
+print
+("Total volume of juice sold:", Total, "Liters")
+
+
+Total_int = int(Total)
+print("Total volume (integer form):", Total_int,"Liters")
+
+Total_str = str(Total)
+print(" Shop sold a total of " + Total_str + " Liters of Juice today.")
+
+import random
+bonus = random.randint(5,10)
+final_Total = Total + bonus
+print
+("final total after adding the bonus Liters:", final_Total, "Liters")
+      
---
+++ classwok.py/jjfvjj.py
@@ -0,0 +1,8 @@
+
+a = "Welcome to Mashup Stack"  
+b = 'Welcome to Mashup Stack'
+#this is how you assign a string to a variable, variable name followed by equal sign
+print(a)
+print(b)
+print(type(a))
+print(type(b))
