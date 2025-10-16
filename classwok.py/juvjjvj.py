0e1563710ce582fd56d8257a17e5a21778c95f2e
akshara <aksharapsreekumar@gmail.com>
2025-10-16 08:53:18 +0530

    update homework/day3home.py

---
+++ classwok.py/juvjjvj.py
@@ -0,0 +1,14 @@
+
+a = 1001
+b = 1000 + 1
+
+print("a == b",a == b)
+print("a is b =", a is b)
+print (id(a))
+print (id(b))
+c=["akshara"]
+d=["akshara"]
+print(id(c))
+print(id(d))
+print(c is d)
+
