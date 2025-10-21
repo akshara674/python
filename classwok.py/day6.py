10ae93857aa63172763f7b5a558f103d79cb2507
akshara <aksharapsreekumar@gmail.com>
2025-10-21 20:00:34 +0530

    update classwok.py/day6.py

---
+++ classwok.py/day6.py
@@ -0,0 +1,19 @@
+
+attendance = [18, 20, 19, 15, 21]
+
+full_days = 0
+total_attendance = 0
+
+
+for students in attendance:
+    total_attendance += students  
+
+    
+    if students >= 20:
+        print("Full")
+        full_days += 1
+    else:
+        print("Not Full")
+
+print("\nNumber of full days:", full_days)
+print("Total attendance over 5 days:", total_attendance)
